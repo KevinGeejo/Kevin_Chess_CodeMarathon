94f74b31965c47f1200f306a12984728403cb3ad
Kevin <74069952+KevinGeejo@users.noreply.github.com>
2024-08-26 10:33:04 +0530

    Add files via upload

---
+++ Backend/DOTNET/ChessCodeMarathon/DAO/PlayerDaoImplementation.cs
@@ -0,0 +1,177 @@
+﻿using ChessCodeMarathon.Models;
+using Npgsql;
+using System.Data;
+using NpgsqlTypes;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.FileSystemGlobbing;
+using System.Numerics;
+
+namespace ChessCodeMarathon.DAO
+{
+    public class PlayerDaoImplementation : IPlayerDao
+    {
+        NpgsqlConnection _connection;
+        public PlayerDaoImplementation(NpgsqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task<List<Player>> GetPlayersByCountry(string country, string cname)
+        {
+            //Retrieve all players from a specific country, sorted by their current world ranking.The
+            //accepts two parameters.
+            Console.WriteLine("C"+country + " SC" + cname);
+            List<Player> plist = new List<Player>();
+            string query = @"select * from chess.Players where country ILIKE @country order by @cname";
+            string errMessage = string.Empty;
+            Player p = null;
+            try
+            {
+                await _connection.OpenAsync();
+                NpgsqlCommand command = new NpgsqlCommand(query, _connection);
+                command.Parameters.AddWithValue("country", country);
+                command.Parameters.AddWithValue("cname", cname);
+                command.CommandType = CommandType.Text;
+                NpgsqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        p = new Player();
+                        p.PlayerId = reader.GetInt32(0);
+                        p.FirstName = reader.GetString(1);
+                        p.LastName = reader.GetString(2);
+                        p.Country = reader.GetString(3);
+                        p.CurrentWorldRanking = reader.GetInt32(4);
+                        p.TotalMatchesPlayed = reader.GetInt32(5);
+                        plist.Add(p);
+                    }
+                }
+                reader.Close();
+            }
+            catch (NpgsqlException e)
+            {
+                errMessage = e.Message;
+                Console.WriteLine("------Exception-----:" + errMessage);
+            }
+
+            return plist;
+        }
+
+        public async Task<int> AddMatch(MatchItem match)
+        {
+            string query = @"insert into chess.Matches(player1_id, player2_id, match_date, match_level, winner_id) values (@player1, @player2, @date, @level, @winner);";
+            string errMessage = string.Empty;
+            try
+            {
+                await _connection.OpenAsync();
+                NpgsqlCommand command = new NpgsqlCommand(query, _connection);
+                command.Parameters.AddWithValue("@player1", match.Player1Id);
+                command.Parameters.AddWithValue("@player2", match.Player2Id);
+                command.Parameters.AddWithValue("@date", DateOnly.Parse( match.Date));
+                command.Parameters.AddWithValue("@level", match.MatchLevel);
+                command.Parameters.AddWithValue("@winner", match.WinnerId);
+                command.CommandType = CommandType.Text;
+                await command.ExecuteNonQueryAsync();
+            }
+            catch (NpgsqlException e)
+            {
+                errMessage = e.Message;
+                Console.WriteLine("------Exception-----:" + errMessage);
+                return 0;
+            }
+            return 1;
+        }
+
+        public async Task<List<PlayerPerformanceView>> GetPlayersPerformance()
+        {
+            List<PlayerPerformanceView> plist = new List<PlayerPerformanceView>();
+            string query = @"SELECT 
+                            p.first_name || ' ' || p.last_name AS full_name,
+                            COUNT(m.match_id) AS total_matches_played,
+                            SUM(CASE WHEN m.winner_id = p.player_id THEN 1 ELSE 0 END) AS total_matches_won,
+                            ROUND(
+                                100.0 * SUM(CASE WHEN m.winner_id = p.player_id THEN 1 ELSE 0 END) / COUNT(m.match_id), 
+                                2
+                            ) AS win_percentage
+                               FROM 
+                             chess.Players p
+                            LEFT JOIN 
+                            chess.Matches m ON p.player_id IN (m.player1_id, m.player2_id)
+                            GROUP BY 
+                            p.player_id, p.first_name, p.last_name;";
+            string errMessage = string.Empty;
+            PlayerPerformanceView p = null;
+
+            try
+            {
+                await _connection.OpenAsync();
+                NpgsqlCommand command = new NpgsqlCommand(query, _connection);
+                command.CommandType = CommandType.Text;
+                NpgsqlDataReader reader = await command.ExecuteReaderAsync();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        p = new PlayerPerformanceView();
+                     
+                        p.FullName = (string)reader[0];
+                        p.TotalMatchesPlayed = reader.GetInt32(1);
+                        p.TotalWon = (Int64)reader[2];
+                        p.WinPercentage = (decimal)reader[3];
+                        plist.Add(p);
+                    }
+                }
+                reader.Close();
+            }
+            catch (NpgsqlException e)
+            {
+                errMessage = e.Message;
+                Console.WriteLine("------Exception-----:" + errMessage);
+            }
+
+            return plist;
+        }
+
+
+        public async Task<List<PlayerPerformanceView>> GetPlayersPerformanceAboveAverage()
+        {
+            List<PlayerPerformanceView> plist = new List<PlayerPerformanceView>();
+            string query = @"SELECT * FROM chess.PlayerPerformance WHERE total_won > (SELECT AVG(total_won) FROM chess.PlayerPerformance);";
+            string errMessage = string.Empty;
+            PlayerPerformanceView p = null;
+
+            try
+            {
+                await _connection.OpenAsync();
+                NpgsqlCommand command = new NpgsqlCommand(query, _connection);
+                command.CommandType = CommandType.Text;
+                NpgsqlDataReader reader = await command.ExecuteReaderAsync();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        p = new PlayerPerformanceView();
+                        p.PlayerId = (int)reader[0];
+                        p.FullName = (string)reader[1];
+                        p.WinPercentage = (decimal)reader[2];
+                        p.TotalWon = (Int64)reader[3];
+                        p.TotalMatchesPlayed = reader.GetInt32(4);
+                        plist.Add(p);
+                    }
+                }
+                reader.Close();
+            }
+            catch (NpgsqlException e)
+            {
+                errMessage = e.Message;
+                Console.WriteLine("------Exception-----:" + errMessage);
+            }
+
+            return plist;
+        }
+    }
+}
